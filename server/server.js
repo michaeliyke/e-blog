0d3a80e0c5d39f5db20918889f97fb3c3d2eae0f
RadouaneAbn <radouaneabn@gmail.com>
2024-09-09 03:06:04 +0100

    ready to merge with the Test branch

--- server/server.js
+++ server/server.js
@@ -3,10 +3,11 @@ import blogRouter from "./routes/route_blog";
 import userRouter from "./routes/route_user";
 
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || "127.0.0.1";
 
 const app = express();
 app.use(json());
-app.use('/blogs', blogRouter);
-app.use('/users', userRouter);
+app.use("/blogs", blogRouter);
+app.use("/users", userRouter);
 
-app.listen(PORT, () => console.log("listening"));
+app.listen(PORT, HOST, () => console.log("listening"));
