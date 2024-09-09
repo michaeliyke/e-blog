d76fdf7f032fc71913cbca680255b197688dc6a6
RadouaneAbn <radouaneabn@gmail.com>
2024-09-09 05:10:52 +0100

    allow cors + start the server after the database is up

--- server/server.js
+++ server/server.js
@@ -1,13 +1,37 @@
 import express, { json } from "express";
-import blogRouter from "./routes/route_blog";
-import userRouter from "./routes/route_user";
+import blogRouter from "./routes/BlogRouter.js";
+import mongoose from "./engine/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "127.0.0.1";
 
 const app = express();
 app.use(json());
-app.use("/blogs", blogRouter);
-app.use("/users", userRouter);
+app.use(cookieParser());
 
-app.listen(PORT, HOST, () => console.log("listening"));
+// allow cors to enable cross origin requests from port 5000
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5000",
+    credentials: true,
+  })
+);
+
+// check the status of the server
+app.get("/status", (req, res) => {
+  return res.status(200).json({ status: "OK" });
+});
+
+// the blogs route
+app.use("/blogs", blogRouter);
+
+// app.use("/users", userRouter);
+
+// wait until the database is up before running the server
+mongoose.connection.once("open", () => {
+  app.listen(PORT, HOST, () => {
+    console.log("Server is running!");
+  });
+});
