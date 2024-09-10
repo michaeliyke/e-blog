6c8defeb6d46513a31b55d777fe17b3258fb15c9
RadouaneAbn <radouaneabn@gmail.com>
2024-09-10 05:53:03 +0100

    add route to get a page of data (10 posts per page)

--- server/routes/BlogRouter.js
+++ server/routes/BlogRouter.js
@@ -2,18 +2,34 @@ import { Router } from "express";
 import {
   allBlogs,
   createTestPosts,
+  getPageOfBlogs,
+  getPostById,
   // createBlog,
   // updateBlog,
 } from "../controllers/BlogController.js";
+import Post from "../models/blog.js";
 
 const blogRouter = Router();
+
 // get all blogs (just for testing)
 blogRouter.get("/", allBlogs);
-// get 20 fake blogs (just for testing)
-blogRouter.get("/test", createTestPosts);
+
+// create fake posts
+blogRouter.post("/create_db", createTestPosts);
+
+// get a page of posts
+blogRouter.get("/page/:page", getPageOfBlogs);
+
+// get post by its id
+blogRouter.get("/:id", getPostById);
+
+// drop blogs collection
+blogRouter.delete("/", async (req, res) => {
+  await Post.collection.drop();
+  return res.status(200).json({ status: "posts collection droped" });
+});
 
 // blogRouter.post("/", createBlog);
-// blogRouter.get("/:id", getBlog);
 // blogRouter.put("/:id", updateBlog);
 
 export default blogRouter;
