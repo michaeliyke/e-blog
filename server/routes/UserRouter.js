b3fcd92993c06cf18cec8e7adec89065670f0a71
RadouaneAbn <radouaneabn@gmail.com>
2024-09-10 05:52:03 +0100

    add some routes to get the user by id or by ref

--- server/routes/UserRouter.js
+++ server/routes/UserRouter.js
@@ -1,6 +1,33 @@
 import { Router } from "express";
+import {
+  getAllUsers,
+  getUserByRef,
+  getUserById,
+  createFakeUsers,
+  createNewUser,
+} from "../controllers/UserController.js";
+import User from "../models/user.js";
 
 const userRouter = Router();
-// userRouter.get("/", getAllUsers);
-// userRouter.get("/:id", getUser);
+// get all users
+userRouter.get("/", getAllUsers);
+
+// get a user by id
+userRouter.get("/:id", getUserById);
+
+// get a user by href
+userRouter.get("/h/:href", getUserByRef);
+
+// create a new user
+userRouter.post("/", createNewUser);
+
+// create some fake users
+userRouter.post("/create_db", createFakeUsers);
+
+// drops the users collection
+userRouter.delete("/", async (req, res) => {
+  await User.collection.drop();
+  return res.status(200).json({ status: "users collection droped" });
+});
+
 export default userRouter;
