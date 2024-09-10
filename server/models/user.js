a64bfa47faf89442be4f2ada19c1fc30816af357
RadouaneAbn <radouaneabn@gmail.com>
2024-09-10 05:40:10 +0100

    add an href field to the user model

--- server/models/user.js
+++ server/models/user.js
@@ -5,12 +5,14 @@ const userSchema = new mongoose.Schema(
   {
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
+    href: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     posts: { type: [String], default: [] },
     intrests: { type: [Number], default: [] },
     paginationToken: { type: String },
-    profilePicture: { type: String, required: false },
+    profilePicture: { type: String },
+    thumbnail: { type: String },
   },
   { timestamps: true }
 );
