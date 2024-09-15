b3782ea688cc415215a07fbae3342e097f3563e4
cyril <cyrildaniels21+git@gmail.com>
2024-09-15 17:27:40 +0100

    fix bug with import, little code cleanup

--- server/controllers/LikeController.js
+++ server/controllers/LikeController.js
@@ -1,27 +1,6 @@
 import Post from "../models/Post.js";
 import Comment from "../models/Comment.js";
 import { Reply } from "../models/Reply.js";
-// import User from "../models/User.js";
-
-// export const likePost = async (req, res) => {
-// const { postId, userId } = req.body;
-// const post = await Post.findById(postId).exec();
-// const user = await User.findById(userId).exec();
-
-// if (!post || !user) {
-//   return res.status(404).json({ status: "Not found" });
-// }
-
-// post.likes.push({ user });
-// try {
-//   await post.save();
-// } catch (err) {
-//   console.log(err);
-//   return res.status(400).json({});
-// }
-
-// return res.status(200).json({ status: "DONE" });
-// };
 
 export const likeUnlike = async (req, res) => {
   console.log("like");
--- server/routes/LikeRouter.js
+++ server/routes/LikeRouter.js
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import { getLikes, likeUnlike } from "../controllers/LikeController.js";
-import { isAuthenticated } from "../middlewares/AuthMiddleware.js";
+import { getLikes } from "../controllers/LikeController.js";
 
 const likeRouter = Router();
 
-// likeRouter.post("/", isAuthenticated, likeUnlike);
 likeRouter.get("/", getLikes);
 
 export default likeRouter;
