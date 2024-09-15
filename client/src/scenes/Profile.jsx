841d128f6bd818e452d00c9ad8a161bb5d7738a1
RadouaneAbn <radouaneabn@gmail.com>
2024-09-16 00:50:04 +0100

    add the header to the profile page

--- client/src/scenes/Profile.jsx
+++ client/src/scenes/Profile.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Header } from "../components/Header";
 
 export function Profile() {
   const [profile, setProfile] = useState(null);
@@ -32,36 +33,45 @@ export function Profile() {
   }
 
   if (error) {
-    return <div className="text-red-500 text-center">Error: {error.message}</div>;
+    return (
+      <div className="text-red-500 text-center">Error: {error.message}</div>
+    );
   }
 
   return (
-    <div className="max-w-4xl mx-auto mt-14 p-5">
-      {profile && profile.user ? (
-        <div className="bg-white relative shadow-lg rounded-lg p-8">
-          <div className="absolute left-1/2 transform -translate-x-1/2 -top-16">
-            {/* Profile image */}
-            <img
-              src={profile.user.profilePicture.thumbnail || "/default-avatar.png"}
-              alt="Profile"
-               className="w-32 h-32 rounded-full border-4 border-white"
-            />
-          </div>
+    <>
+      <Header />
+      <div className="max-w-4xl mx-auto mt-14 p-5">
+        {profile && profile.user ? (
+          <div className="bg-white relative shadow-lg rounded-lg p-8">
+            <div className="absolute left-1/2 transform -translate-x-1/2 -top-16">
+              {/* Profile image */}
+              <img
+                src={
+                  profile.user.profilePicture.thumbnail || "/default-avatar.png"
+                }
+                alt="Profile"
+                className="w-32 h-32 rounded-full border-4 border-white"
+              />
+            </div>
 
-		   {/* Edit profile button */}
-          <button className="absolute top-6 right-6 bg-blue-700 text-white px-4 py-2 rounded-md hover:bg-blue-800 focus:outline-none">
-            Edit profile
-          </button>
+            {/* Edit profile button */}
+            <button className="absolute top-6 right-6 bg-blue-700 text-white px-4 py-2 rounded-md hover:bg-blue-800 focus:outline-none">
+              Edit profile
+            </button>
 
             {/* User info */}
             <div className="flex items-center flex-col my-14">
-              <h2 className="text-2xl font-semibold">{profile.user.firstname} {profile.user.lastname}</h2>
+              <h2 className="text-2xl font-semibold">
+                {profile.user.firstname} {profile.user.lastname}
+              </h2>
               <p className="text-gray-600">{profile.user.email}</p>
             </div>
-        </div>
-      ) : (
-        <p className="text-center text-gray-500">No profile data found.</p>
-      )}
-    </div>
+          </div>
+        ) : (
+          <p className="text-center text-gray-500">No profile data found.</p>
+        )}
+      </div>
+    </>
   );
 }
