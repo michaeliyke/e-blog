ba49b32e414f21fce8a92c84a517ac4cdfba4f99
RadouaneAbn <redouane1997.ocs@gmail.com>
2024-09-24 04:35:21 +0100

    stop checking for unactive connection to database

--- server/engine/db.js
+++ server/engine/db.js
@@ -7,8 +7,7 @@ const MongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017";
 // connect to the data base using mongoose
 mongoose
   .connect(MongoUrl, {
-    connectTimeoutMS: 10000,
-    socketTimeoutMS: 45000,
+    connectTimeoutMS: 60000,
     retryWrites: true,
     w: "majority",
     appName: "e-blog-db",
