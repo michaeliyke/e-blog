cf19bffb2e5673145d13a7ac78f99317f2db6ea5
Michael Iyke <ymichaelc@gmail.com>
2024-09-25 07:48:00 +0100

    Fixed a null error stopping post from loading if user is not logged in

--- client/src/components/ReadPost.jsx
+++ client/src/components/ReadPost.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useRef, useState } from "react";
-import PropTypes from "prop-types";
-import { isUserOwner, request } from "../util/Tools";
-import { Comments } from "./Comments";
-import { useSelector } from "react-redux";
-import { PostOptions } from "./PostOptions";
-import { BookmarkButton, LikeButton } from "./PostStats";
-import { FaRegComment } from "react-icons/fa";
+import { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import { isUserOwner, request } from '../util/Tools';
+import { Comments } from './Comments';
+import { useSelector } from 'react-redux';
+import { PostOptions } from './PostOptions';
+import { BookmarkButton, LikeButton } from './PostStats';
+import { FaRegComment } from 'react-icons/fa';
 
 export function ReadPost({ post_title }) {
-  const href = useSelector((state) => state.auth?.user.href);
-  const [post, setPost] = useState("");
+  const href = useSelector((state) => state?.auth?.user?.href);
+  const [post, setPost] = useState('');
   const commentsRef = useRef(null);
 
   useEffect(() => {
@@ -33,7 +33,7 @@ export function ReadPost({ post_title }) {
 
   const scrollToComments = () => {
     if (commentsRef.current) {
-      commentsRef.current.scrollIntoView({ behavior: "smooth" });
+      commentsRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
@@ -45,10 +45,15 @@ export function ReadPost({ post_title }) {
             <LikeButton post={post} />
             <button
               className="flex items-center space-x-2"
-              onClick={scrollToComments}
-            >
-              <span role="img" aria-label="comment" className="text-2xl">
-                <FaRegComment size={25} color="black" />
+              onClick={scrollToComments}>
+              <span
+                role="img"
+                aria-label="comment"
+                className="text-2xl">
+                <FaRegComment
+                  size={25}
+                  color="black"
+                />
               </span>
               <span className="text-black font-bold">Comment</span>
             </button>
@@ -56,25 +61,29 @@ export function ReadPost({ post_title }) {
           <BookmarkButton post={post} />
         </div>
         {isUserOwner(href, post.user.href) && (
-          <PostOptions postId={post._id} redirectTo="/" />
+          <PostOptions
+            postId={post._id}
+            redirectTo="/"
+          />
         )}
       </div>
       <div className="col-span-2 whitespace-pre-wrap relative z-20">
         <h5
           className="text-3xl font-bold text-center mb-5 px-4 overflow-wrap-break-word"
-          style={{ overflowWrap: "break-word", wordWrap: "break-word" }}
-        >
+          style={{ overflowWrap: 'break-word', wordWrap: 'break-word' }}>
           {post.title}
         </h5>
         <div>
           {post.cover?.image && (
-            <img src={post.cover?.image} className="w-auto h-auto" />
+            <img
+              src={post.cover?.image}
+              className="w-auto h-auto"
+            />
           )}
         </div>
         <p
           className="whitespace-pre-wrap font-serif text-[#333333] font-bold
-      border-t-2 border-gray-500 pt-5 mt-5"
-        >
+      border-t-2 border-gray-500 pt-5 mt-5">
           {post.text}
         </p>
         <div ref={commentsRef}>
